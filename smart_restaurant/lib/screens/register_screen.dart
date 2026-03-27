496b8a1ba447a3daaa616bbb1adee327ec44e095
JameSupakorn13 <jzjmiekung@gmail.com>
2026-03-28 00:39:51 +0700

    สร้างหน้า Dashboard โหมดแอดมินเสร็จแล้ว

--- smart_restaurant/lib/screens/register_screen.dart
+++ smart_restaurant/lib/screens/register_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 import 'home_screen.dart';
-import 'all_reviews_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
